b980ace379f41244e4db672a0bce8a1f87e4720b
Nicole Mills <nam0068@auburn.edu>
2022-04-17 22:11:30 -0500

    create hist manager

--- WebBrowser.UI/HistoryManagerForm.Designer.cs
+++ WebBrowser.UI/HistoryManagerForm.Designer.cs
@@ -29,32 +29,79 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.histSearchBox = new System.Windows.Forms.TextBox();
+            this.histSearchBtn = new System.Windows.Forms.Button();
+            this.histDeleteBtn = new System.Windows.Forms.Button();
+            this.histClearBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(351, 163);
+            this.listBox1.Location = new System.Drawing.Point(161, 158);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.Size = new System.Drawing.Size(416, 199);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // histSearchBox
+            // 
+            this.histSearchBox.Location = new System.Drawing.Point(161, 106);
+            this.histSearchBox.Name = "histSearchBox";
+            this.histSearchBox.Size = new System.Drawing.Size(163, 20);
+            this.histSearchBox.TabIndex = 1;
+            // 
+            // histSearchBtn
+            // 
+            this.histSearchBtn.Location = new System.Drawing.Point(330, 103);
+            this.histSearchBtn.Name = "histSearchBtn";
+            this.histSearchBtn.Size = new System.Drawing.Size(75, 23);
+            this.histSearchBtn.TabIndex = 2;
+            this.histSearchBtn.Text = "Search";
+            this.histSearchBtn.UseVisualStyleBackColor = true;
+            // 
+            // histDeleteBtn
+            // 
+            this.histDeleteBtn.Location = new System.Drawing.Point(411, 103);
+            this.histDeleteBtn.Name = "histDeleteBtn";
+            this.histDeleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.histDeleteBtn.TabIndex = 3;
+            this.histDeleteBtn.Text = "Delete";
+            this.histDeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // histClearBtn
+            // 
+            this.histClearBtn.Location = new System.Drawing.Point(492, 104);
+            this.histClearBtn.Name = "histClearBtn";
+            this.histClearBtn.Size = new System.Drawing.Size(85, 23);
+            this.histClearBtn.TabIndex = 4;
+            this.histClearBtn.Text = "Clear History";
+            this.histClearBtn.UseVisualStyleBackColor = true;
             // 
             // HistoryManagerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.histClearBtn);
+            this.Controls.Add(this.histDeleteBtn);
+            this.Controls.Add(this.histSearchBtn);
+            this.Controls.Add(this.histSearchBox);
             this.Controls.Add(this.listBox1);
             this.Name = "HistoryManagerForm";
             this.Text = "HistoryManagerForm";
             this.Load += new System.EventHandler(this.HistoryManagerForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox histSearchBox;
+        private System.Windows.Forms.Button histSearchBtn;
+        private System.Windows.Forms.Button histDeleteBtn;
+        private System.Windows.Forms.Button histClearBtn;
     }
 }
